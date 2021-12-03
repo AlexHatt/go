dd4f30e4e6dd72948efe5818942e9100a985aff6
@AlexHatt <94867315+AlexHatt@users.noreply.github.com>
2021-12-03 09:51:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+word = input("Type any word:\n")
+
+print("There are", len(word),"characters in %s." % word)
+
+print("\n")
+word2 = input("Type another word:\n")
+print(word2,"in only lowercase is",word2.lower())
+print(word2,"in only uppercase is",word2.upper())
