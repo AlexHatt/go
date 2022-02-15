0694093ff2bb17ad4716ba512d3786f2eb9ea06a
@AlexHatt <94867315+AlexHatt@users.noreply.github.com>
2022-02-15 14:55:59 +0000

    a lot

--- main.py
+++ main.py
@@ -1,8 +1,85 @@
-word = input("Type any word:\n")
+import tkinter
+from tkinter.ttk import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+from tkinter.font import Font
+import time
 
-print("There are", len(word),"characters in %s." % word)
+#create a Window
+window = tkinter.Tk()
+window.title("Revision App")
+window.resizable(False, False)
+window.configure
 
-print("\n")
-word2 = input("Type another word:\n")
-print(word2,"in only lowercase is",word2.lower())
-print(word2,"in only uppercase is",word2.upper())
+bigfont = Font(family='helvetica', size=16, weight='bold', underline=1)
+
+
+#command for button1
+def openmemory():
+    memorywindow = tkinter.Toplevel(window)
+    memorywindow.title("Memory")
+    memorywindow.resizable(False, False)
+
+#command for button2
+def opensecondarystorage():
+    secondarystoragewindow = tkinter.Toplevel(window)
+    secondarystoragewindow.title("Secondary Storage")
+    secondarystoragewindow.resizable(False, False)
+
+
+#command for button3
+def openstorageunits():
+    storageunitswindow = tkinter.Toplevel(window)
+    storageunitswindow.title("Units of Storage")
+    storageunitswindow.resizable(False, False)
+
+#command for button4
+def opendatastorage():
+    datastoragewindow = tkinter.Toplevel(window)
+    datastoragewindow.title("Data Storage")
+    datastoragewindow.resizable(False, False)
+
+#command for button5
+def openrandomiser():
+    randomiserwindow = tkinter.Toplevel(window)
+    randomiserwindow.title("Randomiser")
+    randomiserwindow.resizable(False, False)
+
+#create a title
+label1 = tkinter.Label(
+    window, text="Revise Computer Science\nWelcome to the app").grid(row=0,
+                                                                     column=0)
+
+#create Button
+button1 = tkinter.Button(window,
+                         text="Memory",
+                         bg='violet',
+                         command=openmemory).grid(row=1, column=0, sticky="ew")
+
+button2 = tkinter.Button(window,
+                         text="Secondary Storage",
+                         bg='silver',
+                         command=opensecondarystorage).grid(row=2,
+                                                            column=0,
+                                                            sticky="ew")
+
+button3 = tkinter.Button(window,
+                         text="Units of Storage",
+                         bg='orange',
+                         command=openstorageunits).grid(row=3,
+                                                        column=0,
+                                                        sticky="ew")
+
+button4 = tkinter.Button(window,
+                         text="Data Storage",
+                         bg='lightgreen',
+                         command=opendatastorage).grid(row=1,
+                                                       column=1,
+                                                       sticky="ew")
+
+button5 = tkinter.Button(window, 
+                         text="Topic Randomiser",
+                         bg='beige',
+                         command=openrandomiser).grid(row=2,
+                                                      column=1, 
+                                                      sticky="ew")
